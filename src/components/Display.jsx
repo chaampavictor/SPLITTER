9863548641d44b8fea0ddca53413128b7f5304bb
twatwa98 <victor@gojabu.com>
2023-05-02 04:15:37 -0700

    displaying calculated tips

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,30 +1,29 @@
-const Display = () => {
+const Display = ({ calculatedTip, total, people, handleReset }) => {
+
+    const returnCurrencyAmt = (amt) => new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(amt);
+  
     return (
-        <div className="display">
-            <div className="display-group">
-            <div className="display-row">
-
-                <div className="display-label">
-                    <p className="header">Total</p>
-                    <p className="unit">/ person</p>
-                </div>
-
-                <p className="display-amt">$0.00</p>
-            </div>
-            <div className="display-row">
-                <div className="display-label">
-                    <p className="header">Tip Amount</p>
-                    <p className="unit">/ person</p>
-                </div>
-
-                <p className="display-amt">$0.00</p>
-
-            </div>
-            </div>
-            <button className="btn">Reset</button>
-
+      <div className="display">
+        <div className="display-group">
+          <div className="display-row">
+              <div className="display-label">
+                <p className="header">Tip Amount</p>
+                <p className="unit">/ person</p>
+              </div>
+            <p className="display-amt">{returnCurrencyAmt(`${calculatedTip ? calculatedTip/people : "0"}`)}</p>
+          </div>
+          <div className="display-row">
+              <div className="display-label">
+                <p className="header">Total</p>
+                <p className="unit">/ person</p>
+              </div>
+            <p className="display-amt">{returnCurrencyAmt(`${total ? total/people : "0"}`)}</p>
+          </div>
         </div>
+        {
+          total ? (<button className="btn" onClick={handleReset}>Reset</button>) : (<button className="btn" disabled>Reset</button>)
+        }
+      </div>
     )
-}
-
-export default Display;
+  }
+  export default Display
