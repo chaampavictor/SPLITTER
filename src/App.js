8df9798d5a3981c80e914451d7085051089a5ea9
chaampavictor <32095810+chaampavictor@users.noreply.github.com>
2023-05-03 02:23:45 -0700

    Update App.js
    fixed syntax error bug

--- src/App.js
+++ src/App.js
@@ -1,17 +1,51 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import logo from "./assets/logo.svg"
 import Form from './components/Form'
 import Display from './components/Display'
+
+
 function App() {
+  const [amount, setAmount] = useState("");
+  const [tip, setTip] = useState("");
+  const [people, setPeople] = useState("");
+  const [calculatedTip, setCalculatedTip] = useState(0);
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    if(amount > 0 && people > 0 && tip > 0){
+      setCalculatedTip(amount * (tip / 100));
+      setTotal(calculatedTip + amount)
+    }
+  }, [amount, people, tip, calculatedTip])
+
+  const handleResetBtn = (e) => {
+    setAmount("");
+    setTip("");
+    setPeople("");
+    setCalculatedTip(0);
+    setTotal(0);
+  }
+
   return (
     <div className="wrapper">
-                <img src={logo} alt="Splitter Logo"  />
-      <div className='container'>
-        <Form/>
-      <Display/>
+      <img src={logo} alt="Splitter Logo" />
+      <div className="container">
+        <Form
+          amount={amount}
+          setAmount={setAmount}
+          setTip={setTip}
+          people={people}
+          setPeople={setPeople}
+        />
+        <Display
+          total={total}
+          people={people}
+          calculatedTip={calculatedTip}
+          handleResetBtn={handleResetBtn}
+        />
       </div>
-    </div>                
-  );
+    </div>
+  )
 }
 
 export default App;
